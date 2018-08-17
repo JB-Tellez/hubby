2603f332688cc1f9efa404f3490157babecd2618
JB Tellez <jb.tellez@gmail.com>
2018-08-17 00:52:27 -0700

    uses .env

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const pg = require('pg');
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
+const client = new pg.Client(DATABASE_URL);
+client.connect();
 
 app.use(express.static('./public'));
+
+app.get('/api/candy', (request, response) => {
+  client.query('SELECT * FROM candy;')
+    .then(results => {
+      response.send(results.rows);
+    }).catch(error => {
+      response.status(500).send(error);
+    })
+});
 
 app.listen(PORT, () => console.log('Listening on PORT', PORT));
